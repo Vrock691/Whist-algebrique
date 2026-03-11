cb46c93489b929a6087893da99dfefccaabbfc7e
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-03-11 09:40:54 +0100

    Complete initial push

---
+++ server/src/main/java/fr/valentinmary/whist_algebrique_server/WhistAlgebriqueServerApplication.java
@@ -0,0 +1,13 @@
+package fr.valentinmary.whist_algebrique_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhistAlgebriqueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhistAlgebriqueServerApplication.class, args);
+	}
+
+}
